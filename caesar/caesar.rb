dc83e8a4eb0eb732b3938f2f8828590ffd7c3066
xuyao <xy9123@126.com>
2018-07-13 09:53:06 +0800

    add caesar cipher

---
+++ caesar/caesar.rb
@@ -0,0 +1,36 @@
+class Caesar
+  attr_reader :key	
+  PLAIN_ALPHABET = [*('a'..'z')]
+  CIPHER_ALPHABET = [*('A'..'Z')]
+
+  def initialize key
+   	@key = key
+  end	
+
+  def encoder message
+  	cipher = []
+  	message.split('').each do |m|
+  		cipher << CIPHER_ALPHABET[(PLAIN_ALPHABET.index(m) + @key)%26]
+  	end
+  	cipher.join		
+  end
+  
+  def decoder cipher
+  	message = []
+  	cipher.split('').each do |m|
+  	  message << PLAIN_ALPHABET[(CIPHER_ALPHABET.index(m) - @key)%26]	
+  	end	
+  	message.join
+  end
+
+  def self.dict_attack cipher
+  	26.times do |key|
+  	  message = []
+	  cipher.split('').each do |m|
+	    message << PLAIN_ALPHABET[(CIPHER_ALPHABET.index(m) - key)%26]	
+	  end	
+	  puts "#{cipher} -> 第#{key+1}次破解 -> #{message.join}"	
+  	end	
+  end	
+
+end	
